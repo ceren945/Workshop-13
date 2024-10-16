b7213d222ca83e7d86ad4614326d2ec98ca0d037
ceren.durmus <cerengul2000945@gmail.com>
2024-10-16 09:27:30 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
+import { TodoComponent } from './components/todo/todo.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TodoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'todo-app';
 }
+
+
